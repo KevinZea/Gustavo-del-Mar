3dfe45f211f8edf479cd97cfc94a880b494be25e
Kevin Zea <kevinstevenzeasuarez@gmail.com>
2025-01-09 12:27:06 -0500

    add tag of whatsapp and edit button for converts

--- src/components/BWhatsapp/BWhatsapp.jsx
+++ src/components/BWhatsapp/BWhatsapp.jsx
@@ -6,7 +6,14 @@ import { AiOutlineWhatsApp } from "react-icons/ai"
 export default function BWhatsapp() {
     return (
         <div className='boton'>
-            <a href="https://api.whatsapp.com/send?phone=573118284067" target="_blank" rel="noopener noreferrer">
+            {/* <a href="https://api.whatsapp.com/send?phone=573118284067" target="_blank" rel="noopener noreferrer">
+                <AiOutlineWhatsApp className='iconoWhatsapp'></AiOutlineWhatsApp>
+            </a> */}
+            <a onclick="return gtag_report_conversion('https://api.whatsapp.com/send?phone=573118284067');"
+                href="https://api.whatsapp.com/send?phone=573118284067"
+                target='_blank'
+                rel='noopener noreferrer'
+            >
                 <AiOutlineWhatsApp className='iconoWhatsapp'></AiOutlineWhatsApp>
             </a>
         </div>
